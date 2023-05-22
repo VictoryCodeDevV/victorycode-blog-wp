f4307c7c9b951e8c87c34cd63582fdf2fdb8b2e5
Viktor <76835392+Ra4inA@users.noreply.github.com>
2023-05-22 13:38:48 +0300

    Переписал код сокрытия меню с css на js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 // Main menu
 
+// $(window).ready(function(){
+//     if($(window).width() < 1200) {
+//         $('#mainMenu').addClass('nav_invisible');
+//         $('#menu').addClass('sidebar-left_pos_absolute')
+//     } else {
+//         $('#mainMenu').removeClass('nav_invisible');
+//         $('#menu').removeClass('sidebar-left_pos_absolute')
+//     }
+// });
+
+
+// $(window).resize(function(){
+//     if($(window).width() < 1200) {
+//         $('#mainMenu').addClass('nav_invisible');
+//         $('#menu').addClass('sidebar-left_pos_absolute')
+//     } else {
+//         $('#mainMenu').removeClass('nav_invisible');
+//         $('#menu').removeClass('sidebar-left_pos_absolute')
+//     }
+// });
+
+
+
+
+
 $('#mainMenuBtn').click(function() {
     $(this).toggleClass('menu-btn_active')
     $('#mainMenu').toggleClass('nav_visible');
 });
+
+
