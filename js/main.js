3b801c8df52d65f18a4998d8fcd3938f7f72b8cb
Viktor <vitya-sorokin-92@mail.ru>
2023-09-26 21:01:48 -0300

    Главная страница блога
    Добавлены все иконки
    Весь текст

--- js/main.js
+++ js/main.js
@@ -2,32 +2,61 @@
 
 // $(window).ready(function(){
 //     if($(window).width() < 1200) {
-//         $('#mainMenu').addClass('nav_invisible');
-//         $('#menu').addClass('sidebar-left_pos_absolute')
+//         $('.header__container').prepend($('#headerLogo'));
+//         $('#menu #headerLogo').remove()
+
+
+
+//         // $('#mainMenu').addClass('nav_invisible');
+//         // $('#menu').addClass('sidebar-left_pos_absolute')
 //     } else {
-//         $('#mainMenu').removeClass('nav_invisible');
-//         $('#menu').removeClass('sidebar-left_pos_absolute')
+//         $('#menu .nav__list').prepend($('#headerLogo'));
+//         $('#menu #headerLogo').remove()
+//         // $('#mainMenu').removeClass('nav_invisible');
+//         // $('#menu').removeClass('sidebar-left_pos_absolute')
 //     }
 // });
 
 
 // $(window).resize(function(){
 //     if($(window).width() < 1200) {
-//         $('#mainMenu').addClass('nav_invisible');
-//         $('#menu').addClass('sidebar-left_pos_absolute')
+//         $('.header__container').prepend($('#headerLogo'));
+//         $('#menu .nav__list #headerLogo').remove()
+
+
+
+//         // $('#mainMenu').addClass('nav_invisible');
+//         // $('#menu').addClass('sidebar-left_pos_absolute')
 //     } else {
-//         $('#mainMenu').removeClass('nav_invisible');
-//         $('#menu').removeClass('sidebar-left_pos_absolute')
+//         $('#menu .nav__list').prepend($('#headerLogo'));
+//         $('#menu #headerLogo').remove()
+//         // $('#mainMenu').removeClass('nav_invisible');
+//         // $('#menu').removeClass('sidebar-left_pos_absolute')
 //     }
 // });
 
 
 
 
+// Menu ---------------------------------
 
 $('#mainMenuBtn').click(function() {
     $(this).toggleClass('menu-btn_active')
     $('#mainMenu').toggleClass('nav_visible');
 });
 
+
+ // POPUP ---------------------------------
+
+$('.contacts-popup').click(function () {
+    $('.popup-wrapper').addClass('_popup-show');
+
+    $('body').addClass('_scroll-lock')
+});
+
+$('.popup__btn').click(function () {
+    $('.popup-wrapper').removeClass('_popup-show');
+
+    $('body').removeClass('_scroll-lock');
+});
 
